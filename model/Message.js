922e70e85573df042bafcc5caf74ba9dafde6830
Phuc Nguyen Lamp <giaphuc29082004@gmail.com>
2024-04-01 01:18:42 +0700

    v9

--- model/Message.js
+++ model/Message.js
@@ -14,7 +14,7 @@ const MessageSchema = new Schema({
 // Virtual for message's time
 MessageSchema.virtual("time").get(function () {
     // add 7 utc to get vietnam time
-    return day().add(7, "hour").format("HH:mm");
+    return day(this.date).add(7, "hour").format("HH:mm");
 });
 
 // Export model
